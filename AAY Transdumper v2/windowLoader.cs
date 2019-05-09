8f3da23ec4d7959742712a5afc58c4dcdcf3ca6a
Joshua Vanderzee <vanderzee24@yahoo.com>
2019-05-09 13:35:46 -0400

    fix git index

--- AAY Transdumper v2/windowLoader.cs
+++ AAY Transdumper v2/windowLoader.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,36 @@ using System.Threading.Tasks;
 
 namespace AAY_Transdumper_v2
 {
-    static class windowLoader
+    static class WindowLoader
     {
+        private static List<System.Windows.Window> views = new List<System.Windows.Window>();
+        public static void setMainWindow(System.Windows.Window window)
+        {
 
+            if (views.Find(name => name.Equals(window)) == null)
+                views.Add(window);
+        }
+
+        public static System.Windows.Window createMainWindow(Type window, bool dialogBox = false)
+        {
+            System.Windows.Window temp = views.Find(name => name.GetType().Equals(window));
+            if (temp == null)
+            {
+                temp = (System.Windows.Window)Activator.CreateInstance(window);
+                views.Add(temp);
+            }
+            temp.Closed += onClose;
+            if (dialogBox)
+                temp.ShowDialog();
+            else
+                temp.Show();
+            temp.Focus();
+            return temp;
+        }
+
+        private static void onClose(object sender, EventArgs e)
+        {
+            views.Remove((System.Windows.Window)sender);
+        }
     }
 }
